0165cbd1f080a68a60731bed856ed768546a5b83
fumerajiggy <fumerajiggy29@gmail.com>
2026-08-23 10:09:02 +0800

    Add scrolling road background to CarGame Form

--- CarGame/Form1.cs
+++ CarGame/Form1.cs
@@ -12,6 +12,12 @@ namespace CarGame
 {
     public partial class Form1 : Form
     {
+        private Timer timerRoad;
+        private Image roadImage;
+        private int roadWidth;
+        private int roadHeight;
+        private float roadY;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +28,7 @@ namespace CarGame
         {
             InitilizeWindow();
             RegisterEvents();
+            InitializeRoad();
         }
 
         private void InitilizeWindow()
@@ -34,6 +41,17 @@ namespace CarGame
             KeyPreview = true;
         }
 
+        private void InitializeRoad()
+        {
+            roadImage = Properties.Resources.road;
+            roadWidth = ClientSize.Width;
+            roadHeight = ClientSize.Height;
+
+            timerRoad = new Timer();
+            timerRoad.Interval = 30;
+            timerRoad.Tick += TimerRoad_Tick;
+            timerRoad.Start();
+        }
         private void RegisterEvents()
         {
             Paint += Form1_Paint;
@@ -43,29 +61,44 @@ namespace CarGame
             KeyDown += Form1_KeyDown;
         }
 
+        private void TimerRoad_Tick(object sender, EventArgs e)
+        {
+            roadY += 5;
+
+            if (roadY >= roadHeight)
+                roadY -= roadHeight;
+
+            if (roadY < 0)
+                roadY += roadHeight;
+
+            Invalidate();
+
+        }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            throw new NotImplementedException();
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            throw new NotImplementedException();
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            throw new NotImplementedException();
+            DrawRoad(e.Graphics);
         }
+        private void DrawRoad(Graphics g)
+        {
+            g.DrawImage(roadImage, 0, roadY, roadWidth, roadHeight);
+            g.DrawImage(roadImage, 0, roadY - roadHeight, roadWidth, roadHeight);
+        }
+
     }
 }
